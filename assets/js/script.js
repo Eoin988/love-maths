1cf24c4d45f96b033c0bf72bd440c8a91b924865
Eoin988 <eoin988@gmail.com>
2022-12-20 14:53:21 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
@@ -19,11 +19,17 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * The main Game #Loop,
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
 
-
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType} Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -42,7 +48,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
